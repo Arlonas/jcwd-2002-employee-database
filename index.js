d8e1699575f0129c30318d8f01a04cb90df0110f
Arlonas <hemapalaalfredo@gmail.com>
2022-02-10 14:54:09 +0700

    ui: added new fieldset, named: Sort Employee

---
+++ index.js
@@ -0,0 +1,10 @@
+// Duplikasi  method .find
+// Duplikasi method  .indexOf
+
+// Conditions (indexOf)
+// 1. Mencari index dari sebuah item dalam array
+// 2. Akan return sebuah number (index)
+
+// Condtions (find)
+// 1. Mencari sebuah value (item) dalam array yang memenuhi condition yang ktia berikan
+// 2. Callback function kita 
