60d351227c5d1f1c5fcead103b9b14905f576be8
Arlonas <hemapalaalfredo@gmail.com>
2022-02-10 15:34:30 +0700

    feat : added duplication method of .find

--- index.js
+++ index.js
@@ -8,3 +8,33 @@
 // Condtions (find)
 // 1. Mencari sebuah value (item) dalam array yang memenuhi condition yang ktia berikan
 // 2. Callback function kita 
+
+const ages = [3, 10, 18, 20, 25, 27]
+
+
+const duplicateFind = (arr, callback) => {
+    let resultArr = [];
+    
+    // const callback = (val) => {
+    //     return val > 18
+    //   }
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (callback(arr[i], i, arr)) {
+        resultArr.push(arr[i])
+      }
+    }
+  
+    return resultArr;
+
+   
+}
+let filteredData = duplicateFind(ages, (val) => {
+    return val > 18
+  })
+
+
+  console.log(filteredData)
+
+
+
